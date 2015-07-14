fbbc247afd2910ce1f4b31d8a3c350ee9fef4b03
paulruvolo <paullundyruvolo@gmail.com>
2015-07-14 15:43:57 +0000

    added autosetup

--- PiSetupFiles/get_pi_specific.py
+++ PiSetupFiles/get_pi_specific.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 
-data={"e8:de:27:13:0e:63" : {"adhocnet" : "rpi-adhoc-205", "adhocnetbssid" : "AA:AA:AA:AA:AA:AA"}}
+data={"e8:de:27:13:0e:63" : {"adhocnet" : "rpi-adhoc-205", "adhocnetbssid" : "AA:AA:AA:AA:AA:AA", "adhocnetchannel":5}}
 
 
 process = subprocess.Popen(["ifconfig","wlan0"],stdout=subprocess.PIPE)
@@ -17,7 +17,7 @@ if len(sys.argv) < 2:
 	print "USAGE: ./get_pi_specific.py [adhocnet,adhocnetbssid]"
 	exit(-1)
 
-if sys.argv[1] != 'adhocnet' and sys.argv[1] != 'adhocnetbssid':
+if sys.argv[1] != 'adhocnet' and sys.argv[1] != 'adhocnetbssid' and sys.argv[1] != 'adhocnetchannel':
 	print "unknown info identifier " + sys.argv[1]
 	exit(-1)
 
