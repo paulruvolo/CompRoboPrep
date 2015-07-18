4bb65761a06df1e831e755e73b182c15d6fa5878
paulruvolo <paullundyruvolo@gmail.com>
2015-07-18 19:36:05 +0000

    changed defaults

--- PiSetupFiles/pi_control_gui.py
+++ PiSetupFiles/pi_control_gui.py
@@ -22,6 +22,11 @@ DISPLAY_STATE = 0
 SELECT_NETWORK_MODE_STATE = 1
 NETWORK_RECONFIGURE_STATE = 2
 POWER_DOWN_PROMPT_STATE = 3
+VIDEO_MODE_HEADER = 4
+SELECT_VIDEO_MODE = 5
+VIDEO_MODE_RECONFIGURE = 6
+
+VIDEO_MODES = ["320x240 MJPG", "640x480 MJPG", "1024x768 MJPG", "320x240 H264", "640x480 H264", "1024x768 H264"]
 
 MANAGED_MODE = 0
 ADHOC_MODE = 1
@@ -64,8 +69,28 @@ while True:
 			my_message(lcd,"* OLIN-ROBOTICS\n  AD-HOC")
 		else:
 			my_message(lcd,"  OLIN-ROBOTICS\n* AD-HOC")
+	elif state == SELECT_VIDEO_MODE:
+		my_message(lcd,"* " + VIDEO_MODES[video_mode] + "\n  " + VIDEO_MODES[(video_mode+1)%len(VIDEO_MODES)] + "\n")
 	elif state == POWER_DOWN_PROMPT_STATE:
 		my_message(lcd,"Press select to\nShutdown")
+	elif state == VIDEO_MODE_RECONFIGURE:
+		my_message(lcd,"Reconfiguring video\nmode...")
+		system("sudo killall mjpg_streamer")
+		system("sudo killall video_wrapper.sh")
+		system("sudo killall gst-launch-1.0")
+		if VIDEO_MODES[video_mode] == "320x240 MJPG":
+			system('sudo ~pi/start_jpg_stream.sh "-ex sports -y 240 -x 320 -fps 10 -mm matrix -rot 180" &')
+		elif VIDEO_MODES[video_mode] == "640x480 MJPG":
+			system('sudo ~pi/start_jpg_stream.sh "-ex sports -y 480 -x 640 -fps 10 -mm matrix -rot 180" &')
+		elif VIDEO_MODES[video_mode] == "1024x768 MJPG":
+			system('sudo ~pi/start_jpg_stream.sh "-ex sports -y 768 -x 1024 -fps 10 -mm matrix -rot 180" &')
+		elif VIDEO_MODES[video_mode] == "320x240 H264":
+			system('sudo ~pi/video_wrapper.sh "-ex sports -h 240 -w 320 -fps 10 -mm matrix" &')
+		elif VIDEO_MODES[video_mode] == "640x480 H264":
+			system('sudo ~pi/video_wrapper.sh "-ex sports -h 480 -w 640 -fps 10 -mm matrix" &')
+		elif VIDEO_MODES[video_mode] == "1024x768 H264":
+			system('sudo ~pi/video_wrapper.sh "-ex sports -h 768 -w 1024 -fps 10 -mm matrix" &')
+		state = DISPLAY_STATE
 	elif state == NETWORK_RECONFIGURE_STATE:
 		my_message(lcd,"Reconfiguring\nnetwork...")
 		if network_mode == ADHOC_MODE:
@@ -74,13 +99,38 @@ while True:
 		else:
 			proc = subprocess.Popen(["~pi/start_managed.sh"], stdout=subprocess.PIPE, shell=True)
 			(out, err) = proc.communicate()
+		# need to do this in order to update with new IP address (if gstreamer is running)
+		system("sudo killall gst-launch-1.0")
 		state = DISPLAY_STATE
+	elif state == VIDEO_MODE_HEADER:
+		my_message(lcd,"Video Mode Menu")
 
-	if lcd.is_pressed(LCD.RIGHT) and state == DISPLAY_STATE:
+	if lcd.is_pressed(LCD.RIGHT) and state == VIDEO_MODE_HEADER:
+		state = SELECT_VIDEO_MODE
+		video_mode = 0
+	elif lcd.is_pressed(LCD.DOWN) and state == SELECT_VIDEO_MODE:
+		video_mode = (video_mode + 1)% len(VIDEO_MODES)
+	elif lcd.is_pressed(LCD.UP) and state == SELECT_VIDEO_MODE:
+		video_mode = (video_mode - 1)% len(video_modes)
+	elif lcd.is_pressed(LCD.LEFT) and state == SELECT_VIDEO_MODE:
+		state = VIDEO_MODE_HEADER
+	elif lcd.is_pressed(LCD.RIGHT) and state == SELECT_VIDEO_MODE:
+		state = VIDEO_MODE_RECONFIGURE
+	elif lcd.is_pressed(LCD.RIGHT) and state == DISPLAY_STATE:
 		state = SELECT_NETWORK_MODE_STATE
 		network_mode = MANAGED_MODE
-	elif (lcd.is_pressed(LCD.DOWN) or lcd.is_pressed(LCD.UP)) and state == DISPLAY_STATE:
+	elif lcd.is_pressed(LCD.DOWN) and state == DISPLAY_STATE:
+		state = VIDEO_MODE_HEADER
+	elif lcd.is_pressed(LCD.DOWN) and state == VIDEO_MODE_HEADER:
 		state = POWER_DOWN_PROMPT_STATE
+	elif lcd.is_pressed(LCD.DOWN) and state == POWER_DOWN_PROMPT_STATE:
+		state = DISPLAY_STATE
+	elif lcd.is_pressed(LCD.UP) and state == DISPLAY_STATE:
+		state = POWER_DOWN_PROMPT_STATE
+	elif lcd.is_pressed(LCD.UP) and state == VIDEO_MODE_HEADER:
+		state = DISPLAY_STATE
+	elif lcd.is_pressed(LCD.UP) and state == POWER_DOWN_PROMPT_STATE:
+		state = VIDEO_MODE_HEADER
 	elif (lcd.is_pressed(LCD.DOWN) or lcd.is_pressed(LCD.UP)) and state == SELECT_NETWORK_MODE_STATE:
 		if network_mode == MANAGED_MODE:
 			network_mode = ADHOC_MODE
@@ -90,8 +140,6 @@ while True:
 		state = DISPLAY_STATE
 	elif (lcd.is_pressed(LCD.RIGHT) or lcd.is_pressed(LCD.SELECT)) and state == SELECT_NETWORK_MODE_STATE:
 		state = NETWORK_RECONFIGURE_STATE
-	elif (lcd.is_pressed(LCD.UP) or lcd.is_pressed(LCD.DOWN)) and state == POWER_DOWN_PROMPT_STATE:
-		state = DISPLAY_STATE
 	elif (lcd.is_pressed(LCD.SELECT) or lcd.is_pressed(LCD.RIGHT)) and state == POWER_DOWN_PROMPT_STATE:
 		lcd.set_backlight(False)
 		lcd.clear()
--- PiSetupFiles/start_jpg_stream.sh
+++ PiSetupFiles/start_jpg_stream.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+VIDEOMODE="-ex sports -y 480 -x 640 -fps 10 -mm matrix -rot 180"
+echo $#
+if [ $# = 1 ]; then
+	VIDEOMODE=$1
+	echo $VIDEOMODE
+fi
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~pi/mjpg-streamer/mjpg-streamer-experimental
-~pi/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www -p 11111" -i "input_raspicam.so -ex sports -y 240 -x 320 -fps 30 -mm matrix -rot 180"
+~pi/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www -p 11111" -i "input_raspicam.so -quality 20 $VIDEOMODE"
