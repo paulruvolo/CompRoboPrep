a2b595964f55a62a954baf1bc70b6e09ede30835
paulruvolo <paullundyruvolo@gmail.com>
2015-08-25 22:14:38 +0000

    added a simple server to do UDP hole punching

--- PiSetupFiles/echoserver-simple.py
+++ PiSetupFiles/echoserver-simple.py
@@ -4,11 +4,13 @@
 A simple echo server
 """
 
+import subprocess
+import re
 import socket
 from os import system
 from time import sleep
 host = ''
-port = 10002
+port = 10003
 backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,9 +25,19 @@ while 1:
     system('sudo killall gst-launch-1.0')
     system('sudo killall mjpg_streamer')
     cmd  = "~pi/start_gst_udp.sh " + address[0] + ' &'
+    source_port = None
     system(cmd)
-    data = client.recv(size)
-    if data:
-        client.send(data)
+    p = subprocess.Popen(['tcpdump','-i','wlan0','udp','port','5000','-v'], stdout=subprocess.PIPE)
+    while True:
+        line = p.stdout.readline()
+	m = re.search('raspberrypi.local.([0-9]+) ',line)
+	if m:
+            print m.group(1)
+            source_port = m.group(1)
+            p.kill()
+            break
+    print source_port
+    if source_port:
+        client.send(source_port + "\n")
     client.close()
 s.close()
