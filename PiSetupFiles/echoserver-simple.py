2bfc0ca65214ce126c743a26f94db38f41cb28ec
paulruvolo <paullundyruvolo@gmail.com>
2015-10-16 13:49:04 +0000

    updated echo server to add a timeout

--- PiSetupFiles/echoserver-simple.py
+++ PiSetupFiles/echoserver-simple.py
@@ -7,8 +7,9 @@ A simple echo server
 import subprocess
 import re
 import socket
+import select
 from os import system
-from time import sleep
+from time import sleep, time
 host = ''
 port = 10003
 backlog = 5
@@ -37,11 +38,18 @@ while 1:
         send_port = fields[0]
         video_mode = fields[1]
     cmd  = "~pi/start_gst_udp.sh " + address[0] + ' ' + send_port + ' "' + video_mode + '"&'
-    print cmd
     source_port = None
-    system(cmd)
+    print system(cmd)
     p = subprocess.Popen(['tcpdump','-i','wlan0','udp','port',send_port,'-v'], stdout=subprocess.PIPE)
-    while True:
+    poll_obj = select.poll()
+    poll_obj.register(p.stdout, select.POLLIN)  
+    start_time = time()
+    while time() - start_time < 60:
+        print time() - start_time
+        poll_result = poll_obj.poll(0)
+        if not poll_result:
+            sleep(1)
+            continue
         line = p.stdout.readline()
 	m = re.search('raspberrypi.*.local.([0-9]+) ',line)
 	if m:
