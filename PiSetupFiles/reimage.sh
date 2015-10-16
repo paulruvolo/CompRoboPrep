6ded3c349f6f361ad4ba559da4bda6004421b4e5
paulruvolo <paullundyruvolo@gmail.com>
2015-10-16 16:16:26 -0400

    updated goldmaster version

--- PiSetupFiles/reimage.sh
+++ PiSetupFiles/reimage.sh
@@ -7,4 +7,4 @@ fi
 echo image_$1
 sudo diskutil unmountDisk /dev/disk$1
 
-screen -d -m -S image_$1 sudo dd if=~/Desktop/comprobo_2015_goldmaster_1.0.3.img of=/dev/rdisk$1 bs=1m
+screen -d -m -S image_$1 sudo dd if=~/Desktop/comprobo_2015_goldmaster_1.0.4.img of=/dev/rdisk$1 bs=1m
