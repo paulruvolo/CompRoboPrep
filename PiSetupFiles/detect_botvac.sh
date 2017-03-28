5aac942e89861e2b58f27981d10b07276202bfd3
paulruvolo <paullundyruvolo@gmail.com>
2017-03-28 20:03:20 +0000

    minor mods

--- PiSetupFiles/detect_botvac.sh
+++ PiSetupFiles/detect_botvac.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-USB=`lsusb | grep 2108:780c`
+USB=`lsusb | grep 2108:780b`
 if [[ ! -z $USB ]]
 then
-	sudo insmod /lib/modules/3.18.14-v7+/kernel/drivers/usb/serial/usbserial.ko vendor=0x2108 product=0x780C
-else
 	sudo insmod /lib/modules/3.18.14-v7+/kernel/drivers/usb/serial/usbserial.ko vendor=0x2108 product=0x780B
+else
+	sudo insmod /lib/modules/3.18.14-v7+/kernel/drivers/usb/serial/usbserial.ko vendor=0x2108 product=0x780C
 fi
